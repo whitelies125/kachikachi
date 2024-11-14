44a5f5a5045e4337f94eadee4ea86e1f00406868
whitelies125 <lzb125125@gmail.com>
2024-11-15 04:57:16 +0800

    init

---
+++ kachikachi.py
@@ -0,0 +1,43 @@
+import time
+import win32gui
+import win32process
+import psutil
+
+from datetime import datetime
+
+def get_active_window_title():
+    hwnd = win32gui.GetForegroundWindow()  # 获取当前激活窗口的句柄
+    if not hwnd:
+        return None, None
+    # 当前激活窗口的标题
+    window_title = win32gui.GetWindowText(hwnd)  # 获取窗口标题
+    _, pid = win32process.GetWindowThreadProcessId(hwnd)  # 获取窗口所属的进程 ID
+    process = psutil.Process(pid)  # 获取进程对象
+    # 当前激活窗口的进程名
+    process_name = process.name()  # 返回进程名称
+    return window_title, process_name
+
+def main():
+    print("Start tracking active window. Press Ctrl+C to stop.")
+    now = datetime.now()
+    year, month, day = now.year, now.month, now.day
+    # 时间戳
+    pre_time = int(time.time())
+    try:
+        while True:
+            title, process_name = get_active_window_title()
+            print(f"Active window title: {title}, process_name : {process_name}")
+
+            now = datetime.now()
+            year, month, day = now.year, now.month, now.day
+            cur_time = int(time.time())
+            duration = cur_time - pre_time
+            pre_time = cur_time
+            print(f"Year: {year}, Month: {month}, Day: {day}, Timestamp: {pre_time}, duration: {duration}")
+
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nStopped tracking active window.")
+
+if __name__ == "__main__":
+    main()
