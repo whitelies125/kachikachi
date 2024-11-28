5a17a43bb01afde374e773b3a64027134ba76af0
whitelies125 <lzb125125@gmail.com>
2024-11-29 00:41:04 +0800

    add export.py

---
+++ export.py
@@ -0,0 +1,36 @@
+import csv
+import argparse
+from db_manager import DbManager
+from datetime import datetime
+
+def main():
+    parser = argparse.ArgumentParser(description="Export database data.")
+    parser.add_argument(
+        "-origin",
+        action = "store_true",
+        help = "Export data as timestamps if specified, otherwise export as 'YYYY-MM-DD HH:MM:SS'."
+    )
+    args = parser.parse_args()
+
+    dbManager = DbManager()
+    process_tbl = { k: v for k, v in dbManager.get_process_tbl() }
+    activity_tbl = dbManager.get_activity_tbl()
+    dbManager.disconnect()
+
+    data = list()
+    for activity in activity_tbl:
+        db_id, process_id, start_time, end_time = activity
+        if not args.origin:
+            start_time = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
+            end_time = datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
+        data.append((db_id, process_tbl[process_id], start_time, end_time))
+    print(data)
+
+    with open('export.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['id', 'process_name', 'start_time', 'end_time'])
+        for item in data:
+            writer.writerow(item)
+
+if __name__ == "__main__":
+    main()
